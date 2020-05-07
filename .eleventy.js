d7aae340691450a3bb901ffa17d292252872df17
akashrine <julien.brionne@gmail.com>
2020-05-07 14:02:25 +0200

    add nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const pluginRss = require('@11ty/eleventy-plugin-rss')
+const pluginNavigation = require('@11ty/eleventy-navigation')
 const markdownIt = require('markdown-it')
 
 const filters = require('./utils/filters.js')
@@ -6,10 +7,10 @@ const transforms = require('./utils/transforms.js')
 const shortcodes = require('./utils/shortcodes.js')
 const iconsprite = require('./utils/iconsprite.js')
 
-
 module.exports = function (config) {
     // Plugins
     config.addPlugin(pluginRss)
+    config.addPlugin(pluginNavigation)
 
     // Filters
     Object.keys(filters).forEach((filterName) => {
@@ -46,8 +47,6 @@ module.exports = function (config) {
     // Layouts
     config.addLayoutAlias('base', 'base.njk')
     config.addLayoutAlias('post', 'post.njk')
-    config.addLayoutAlias('page', 'page.njk')
-
 
     // Pass-through files
     config.addPassthroughCopy('src/robots.txt')
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,2 +1,8 @@
+// Focus Visible Polyfill
 import 'focus-visible'
+
+// Internal Modules
+import './modules/nav'
+
+// Smooth on scroll effect
 AOS.init();
